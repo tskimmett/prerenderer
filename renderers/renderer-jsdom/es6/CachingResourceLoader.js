5b8b72f1e4f2afc39080aa1c5d31ad0f0d20b91b
Jake Morrison <77012821+jake-cognito@users.noreply.github.com>
2021-01-11 16:12:21 -0500

    Update CachingResourceLoader.js
    Add google fonts to cache

--- renderers/renderer-jsdom/es6/CachingResourceLoader.js
+++ renderers/renderer-jsdom/es6/CachingResourceLoader.js
@@ -6,7 +6,7 @@ module.exports = class CachingResourceLoader extends jsdom.ResourceLoader {
    fetch(url, options) {
       if (url.endsWith(".png") || url.endsWith(".svg") || url.endsWith(".gif") || url.endsWith(".jpg") || url.endsWith(".map") || url.indexOf("undefined") > -1) {
          return null;
-      } else if (url.endsWith(".js") || url.endsWith(".css"))
+      } else if (url.endsWith(".js") || url.endsWith(".css") || url.includes('fonts.googleapis.com/css'))
       {
          if (cache[url]) {
             return cache[url];
